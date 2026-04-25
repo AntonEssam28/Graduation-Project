e973edb38427899917e27e2ecb1c20c5e8067070
AntonEssam28 <antonessam7@gmail.com>
2026-04-25 18:22:31 +0300

    Your update message

--- src/app/(public)/adopt/components/AdaptionForm.tsx
+++ src/app/(public)/adopt/components/AdaptionForm.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { useState, type ChangeEvent, type FormEvent } from "react";
+import { useState, useEffect, type ChangeEvent, type FormEvent } from "react";
+
 import { Dog, HeartHandshake, MapPin, ShieldCheck, Loader2 } from "lucide-react";
 
 type AdoptionFormProps = {
